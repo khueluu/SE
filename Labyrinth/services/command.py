9f9a97b36811288b9193a4e7389e183455da86be
khueluu <luuminhsaokhue@gmail.com>
2021-05-14 23:17:11 +0700

    Add abstract method execute() to IUserCommand

--- Labyrinth/services/command.py
+++ Labyrinth/services/command.py
@@ -4,4 +4,7 @@ class IUserCommand(metaclass = ABCMeta):
     @abstractmethod
     def get_command_tag(self): pass
 
+    @abstractmethod
+    def execute(self): pass
+
     # Returns: (finish?, state, msg)
