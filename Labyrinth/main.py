8eb395e449264dc1d1e1b61e0455d57b117ef6c1
khueluu <luuminhsaokhue@gmail.com>
2021-05-18 23:21:33 +0700

    add load_labyrinth()

--- Labyrinth/main.py
+++ Labyrinth/main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import os
 from impl.commands import *
 from utils import *
 
@@ -16,7 +17,7 @@ def play(lbr):
     except KeyboardInterrupt:
         print("\nQuit game without saving")
 
-def validate(size):
+def validate_size(size):
     try:
         assert (int(size) >= 4) and (int(size) <= 10)
     except:
@@ -29,7 +30,7 @@ def create_labyrinth():
     try:
         while not(finished):
             size = input("$> Please select labyrinth size from 4 to 10: ")
-            is_valid = validate(size)
+            is_valid = validate_size(size)
             if is_valid:
                 lbr = Labyrinth()
                 lbr.create_new(size=size)
@@ -39,11 +40,28 @@ def create_labyrinth():
     except KeyboardInterrupt:
         print("\nQuit game without saving")
         sys.exit()
+
+def load_labyrinth():
+    finished = False
+    try:
+        while not(finished):
+            input_file = input("$> Please type file path to load labyrinth: ")
+            is_valid = validate(os.path.isfile(input_file),"File not found")
+            if is_valid:
+                lbr = Labyrinth()
+                lbr.load(input_file=input_file)
+                print(f"Loaded labyrinth of size {lbr.size}x{lbr.size}")
+                finished = True
+                return lbr
+    except KeyboardInterrupt:
+        print("\nQuit game without saving")
+        sys.exit()
     
 
 def main():
     print("="*20, "Welcome to Labyrith", "="*20)
-    lbr = create_labyrinth()
+    #lbr = create_labyrinth()
+    lbr = load_labyrinth()
     play(lbr)
 
 if __name__ == "__main__":
